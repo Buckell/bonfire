bdc1ef2bbff1159757ae5878e8f558c6f8aa0945
Max Goddard <max@altis.gg>
2024-07-08 04:07:03 -0700

    Added directories registry.

---
+++ src/main/app/directories.ts
@@ -0,0 +1,7 @@
+import path from 'path';
+
+export const DIRECTORIES = {
+    PROJECT_WORKING: path.resolve('project_working'),
+    PROJECTS: path.resolve('projects'),
+    DATA: path.resolve('data'),
+};
